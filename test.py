95f99fe1c0f12498d5e5be69d9a74c1d487e0d8c
yinpengcheng <yinpengcheng>
2018-10-11 19:04:23 +0800

    monkey总结完成

--- test.py
+++ test.py
@@ -8,4 +8,20 @@ device = MonkeyRunner.waitForConnection()
 
 device.press('KEYCODE_HOME', MonkeyDevice.DOWN_AND_UP)
 
-MonkeyRunner.alert(u"我日",u"riri",u"jeje")
+# selectString = MonkeyRunner.alert(u"我日",u"riri",u"jeje")
+
+# selectString = MonkeyRunner.choice("choice a sex", ["man", "woman"], "rorororor")
+
+# print type(selectString)
+
+# print selectString
+
+# selectString = "press"+ selectString
+
+# MonkeyRunner.alert(selectString,u"确认",u"确认")
+
+# MonkeyRunner.help("text")
+
+inputString = MonkeyRunner.input("message","initialvalue","title","oktitle","canceltitle")
+
+print inputString.decode("utf-8")
